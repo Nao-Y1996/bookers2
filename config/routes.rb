c98b94cbf7eff181d65a0eea6841b4c94cf8e48f
Nao-Y1996 <0809n.ysoccer@gmail.com>
2020-03-21 12:27:07 +0900

    maked & modified user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/about', to: 'home#about'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index,:show]
+
 end
